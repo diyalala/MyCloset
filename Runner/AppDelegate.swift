280877249aa80f887a2282552e81a1b170118a8f
diyalala <133143729+diyalala@users.noreply.github.com>
2025-08-03 22:03:38 +0545

    Add files via upload

--- Runner/AppDelegate.swift
+++ Runner/AppDelegate.swift
@@ -1,15 +1,13 @@
-import UIKit
-import Flutter
-import GoogleMaps
+import Cocoa
+import FlutterMacOS
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-     GMSServices.provideAPIKey("AIzaSyAo4NxFrg5xWCfBNzeLD0ZVUADKatE4iMA")
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+class AppDelegate: FlutterAppDelegate {
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
   }
 }
